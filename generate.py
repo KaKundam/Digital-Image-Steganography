fa84e2e5758e834d597b52094c11d1afe2092cf8
papcaii <duyhn2004.work@gmail.com>
2026-01-17 05:46:55 +0700

    tạo file generate

---
+++ generate.py
@@ -0,0 +1,11 @@
+import os
+
+print("=== GENERATE STEGO IMAGES ===")
+
+print("\n[DFT]")
+os.system("python -m methods.embed.DFT")
+
+print("\n[DWT]")
+os.system("python -m methods.embed.DWT")
+
+print("\n[Done] Generation completed.")
